56fedcd11e48e2b88b5c62a07a4da8fa3e744ee3
aditya745 <thakuraditya72@yahoo.in>
2018-10-04 10:21:44 +0300

    github changed

--- client/src/components/AppNavbar.js
+++ client/src/components/AppNavbar.js
@@ -31,7 +31,7 @@ class AppNavbar extends Component {
             <Collapse isOpen={this.state.isOpen} navbar>
               <Nav className="ml-auto" navbar>
                 <NavItem>
-                  <NavLink href="https://github.com/mdsaon">
+                  <NavLink href="https://github.com/aditya745">
                     Github
                   </NavLink>
                 </NavItem>
